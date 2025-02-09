0bc912351a5893fca036db6dd2a7ea306547dd09
Lune162 <lunixbus@gmail.com>
2025-02-09 16:13:21 -0500

    Quick update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js
-import { supabase } from './supabase.js'; // This imports the supabase client
+const supabase = window.supabase.createClient("https://ocjkzxaaqbsmmcveuxxn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jamt6eGFhcWJzbW1jdmV1eHhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMjgzNTEsImV4cCI6MjA1NDcwNDM1MX0.Ha8jMv6pewZAka5NV4N8Tj6olp6LlsqYpJvcgUQW_1w");
 console.log(supabase);
 
 document.addEventListener("DOMContentLoaded", function () {
--- supabase.js
+++ supabase.js
@@ -1,10 +1,6 @@
-// supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@1.35.6/dist/umd/supabase.min.js';
-
 const supabaseUrl = "https://ocjkzxaaqbsmmcveuxxn.supabase.co";  // Replace with your Supabase URL
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jamt6eGFhcWJzbW1jdmV1eHhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMjgzNTEsImV4cCI6MjA1NDcwNDM1MX0.Ha8jMv6pewZAka5NV4N8Tj6olp6LlsqYpJvcgUQW_1w";  // Replace with your Supabase Key
-const supabase = createClient(supabaseUrl, supabaseKey);
-
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 console.log(supabase);
 
 export { supabase };  // Export the client
