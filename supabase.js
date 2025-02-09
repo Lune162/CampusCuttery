61e16decd99c5cc916c89e9dcda6ef41ba1f70b4
Lune162 <lunixbus@gmail.com>
2025-02-09 15:37:27 -0500

    Quick update

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.0.0/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.0.0/dist/umd/supabase.min.js';
 
 const supabaseUrl = "https://ocjkzxaaqbsmmcveuxxn.supabase.co";  // Replace with your Supabase URL
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jamt6eGFhcWJzbW1jdmV1eHhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMjgzNTEsImV4cCI6MjA1NDcwNDM1MX0.Ha8jMv6pewZAka5NV4N8Tj6olp6LlsqYpJvcgUQW_1w";  // Replace with your Supabase Key
