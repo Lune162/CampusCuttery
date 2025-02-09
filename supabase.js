3dc4b69bb104a189730af9da739d9a0ce5c4d612
Lune162 <lunixbus@gmail.com>
2025-02-09 17:12:29 -0500

    Quick update

--- supabase.js
+++ supabase.js
@@ -1,11 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ocjkzxaaqbsmmcveuxxn.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jamt6eGFhcWJzbW1jdmV1eHhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMjgzNTEsImV4cCI6MjA1NDcwNDM1MX0.Ha8jMv6pewZAka5NV4N8Tj6olp6LlsqYpJvcgUQW_1w';
+import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-// Now you can use supabase.from
-supabase.from('your_table').select('*').then(response => {
-  // Handle data
-});
+const supabaseUrl = 'https://ocjkzxaaqbsmmcveuxxn.supabase.co'
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jamt6eGFhcWJzbW1jdmV1eHhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMjgzNTEsImV4cCI6MjA1NDcwNDM1MX0.Ha8jMv6pewZAka5NV4N8Tj6olp6LlsqYpJvcgUQW_1w
+const supabase = createClient(supabaseUrl, supabaseKey)
